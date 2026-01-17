05ed3c0119a063640cb716d3ab379956b6a58e5c
PrithviRajVerma <2266prr@gmail.com>
2026-01-17 16:59:15 +0530

    express type definition updated

--- src/types/session.d.ts
+++ src/types/session.d.ts
@@ -2,7 +2,7 @@ import "express-session";
 
 declare module "express-session" {
     interface SessionData {
-        userId?: number;
+        userId?: string;
         authMethod?: "google" | "email";
 
         // PKCE + CSRF fields
